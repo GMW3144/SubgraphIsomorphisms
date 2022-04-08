Fixed color mapping to work once with number of instances and average matchings

HeatMap/constructHeatMap.py
@@ -2,6 +2,7 @@ import os # read file
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from textwrap import wrap
 
 def readInputDataOneFolder(dataFolder):
     graphInformation = {}  # size - graph info
@@ -164,28 +165,37 @@ def readInputData(dataFolder):
 
     return (graphInformation, maxDe, maxDi)
 
-def constructHeatMap(size, datapoints, minM, maxM, folder, outlierMaxPointsX, outlierMaxPointsY,
-                     outlierMinPointsX, outlierMinPointsY):
-    #plt.imshow(datapoints, cmap='viridis', extent=[1, datapoints.shape[0]+1, 1, datapoints.shape[1]+1],
-    #           vmin = 0, vmax = maxM)
+def constructHeatMap(size, datapoints, minM, maxM, trueMax, folder, outlierMaxPointsX, outlierMaxPointsY,
+                             outlierMaxM, type):
+    # plot the histogram
     sns.heatmap(datapoints, cmap='viridis', cbar=True, vmin=minM, vmax=maxM)
     plt.ylim(1, datapoints.shape[0]+1)
     plt.xlim(1, datapoints.shape[1]+1)
-    #plt.colorbar()
     plt.xlabel('diameter (range x to x+1)')
     plt.ylabel('average degree (range x to x+1)')
-    plt.title("Average Number of Matchings for Hard-to-find Graphs Size "+size)
-    plt.scatter(outlierMaxPointsX, outlierMaxPointsY)
-    plt.scatter(outlierMinPointsX, outlierMinPointsY)
-    plt.savefig(folder+"size"+size+".png")
+    if(type == "M"):
+        title = "Average Number of Matchings for Hard-to-find Graphs Size " + size
+    else:
+        title ="Number of Hard-to-find Graphs Size "+size
+    plt.title('\n'.join(wrap(title, 40)))
 
+    # plot the outliers
+    if(len(outlierMaxM)>0):
+        plt.scatter(outlierMaxPointsX, outlierMaxPointsY, vmin=maxM, vmax=trueMax, c=outlierMaxM, cmap = "gist_heat")
+        plt.colorbar()
+
+    # save and close the plot
+    if(type == "M"):
+        plt.savefig(folder+"size"+size+"_avgMatching.png")
+    else:
+        plt.savefig(folder+"size"+size+"_count.png")
     plt.close()
 
-def plotValues(graphInformation, maxDe, maxDi, folder):
+def plotValues(graphInformation, maxDe, maxDi, folder, type):
     heatMapsInfo = {}
     capMax = 10000000
-    capMin = 0
     maxM = 0
+    trueMax = 0
     minM = -1
 
     outlierMaxInfo = {}
@@ -194,31 +204,31 @@ def plotValues(graphInformation, maxDe, maxDi, folder):
         n = key.split("_")[0][1:]
         if (n not in heatMapsInfo):
             heatMapsInfo[n] = np.zeros((maxDe+1,maxDi+1))
-            outlierMaxInfo[n] = ([], [])
-            outlierMinInfo[n] = ([], [])
+            outlierMaxInfo[n] = ([], [], [])
+            outlierMinInfo[n] = ([], [], [])
         de = int(key.split("_")[1][2:])
         di = int(key.split("_")[2][2:])
 
-        M = graphInformation[key]["M"]
-        if(M!= None and M>capMax):
-            M = capMax
+        val = graphInformation[key][type]
+        if(val!= None and val>capMax):
+            if(val>trueMax):
+                trueMax = val
             outlierMaxInfo[n][0].append(di+.5)
             outlierMaxInfo[n][1].append(de+.5)
-        if(M!=None and M<capMin):
-            M = capMin
-            outlierMinInfo[n][0].append(di+.5)
-            outlierMinInfo[n][1].append(de+.5)
-        if (M!= None and M>maxM):
-            maxM = M
-        if(M!=None and (minM==-1 or M<minM)):
-            minM = M
+            outlierMaxInfo[n][2].append(val)
+            val = capMax
+        if (val!= None and val>maxM):
+            maxM = val
+        if(val!=None and (minM==-1 or val<minM)):
+            minM = val
 
-        heatMapsInfo[n][de][di] = M
+        heatMapsInfo[n][de][di] = val
+
 
 
     for key in heatMapsInfo:
-        constructHeatMap(key, heatMapsInfo[key], minM, maxM, folder, outlierMaxInfo[key][0], outlierMaxInfo[key][1],
-                         outlierMinInfo[key][0], outlierMinInfo[key][1])
+        constructHeatMap(key, heatMapsInfo[key], minM, maxM, trueMax, folder, outlierMaxInfo[key][0], outlierMaxInfo[key][1],
+                         outlierMaxInfo[key][2], type)
 
 if __name__ == '__main__':
     dataFolder = "C:\\Users\\Gabi\\Desktop\\IndependentStudy\\GitHubProject\\Data\\Output\\HeatMap\\Yeast\\NonInduced\\"
@@ -228,5 +238,6 @@ if __name__ == '__main__':
     #plt.rc('xtick', labelsize=textSize)
     #plt.rc('ytick', labelsize=textSize)
 
-    plotValues(graphInformation, maxDe, maxDi, dataFolder+"HeatMaps\\")
+    plotValues(graphInformation, maxDe, maxDi, dataFolder+"HeatMaps\\", "M")
+    plotValues(graphInformation, maxDe, maxDi, dataFolder+"HeatMaps\\", "I")
         